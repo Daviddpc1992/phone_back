093514c227db6bfbfa9086319513a3a7ecb6027e
David <daviddelpozocalderon@gmail.com>
2021-10-24 23:03:59 +0200

    1

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const cors = require('cors');
 
-require('dotenv').config();
+require('.env').config();
 
 
 require('./dbConfig');
